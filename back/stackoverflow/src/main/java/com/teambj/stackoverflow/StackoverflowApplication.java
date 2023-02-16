ce7e5a50f259e01f0133b316812466c5e73eee04
jw_0819 <jw_0819@naver.com>
2023-02-16 14:46:30 +0900

    [init] backend stackoverflow

---
+++ back/stackoverflow/src/main/java/com/teambj/stackoverflow/StackoverflowApplication.java
@@ -0,0 +1,13 @@
+package com.teambj.stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowApplication.class, args);
+	}
+
+}
---
+++ back/stackoverflow/src/test/java/com/teambj/stackoverflow/StackoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teambj.stackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
