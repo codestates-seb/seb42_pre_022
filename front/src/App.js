ed1434bba40fc36ed74fb06ba5c3ea920f423fe0
YUNH7 <yunny126@gmail.com>
2023-02-19 13:41:42 +0900

    [design] Header 구현

--- front/src/App.js
+++ front/src/App.js
@@ -4,18 +4,32 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Nav from "./Components/Nav";
 import Aside from "./Components/Aside";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import { Routes, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 function App() {
+  const state = useSelector(state => state.loginReducer);
+
   return (
     <div className="app-wrap">
       <GlobalStyle />
       <Header />
       <div className="wrap">
-        <Nav />
-        <div className="content">
-          <Questions />
-          <Aside />
-        </div>
+        <Routes>
+          <Route path="/"
+            element={<>
+            <Nav />
+            <div className="content">
+              <Questions />
+              <Aside />
+            </div>
+            </>}
+          />
+          <Route path="/users/login" element={<Login />} />
+          <Route path="/users/signup" element={<Signup />} />
+        </Routes>
       </div>
       <Footer />
     </div>
---
+++ front/src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from "./Reducers/loginReducer";
+
+const store = configureStore({
+  reducer: {
+    loginReducer,
+  }
+})
+
+export default store;
