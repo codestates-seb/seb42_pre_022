db27ab59181b434912096149077107202d4cc8e7
YUNH7 <yunny126@gmail.com>
2023-02-20 09:16:00 +0900

    [style] SearchInput styles 폴더로 이동

--- front/src/Components/Header.js
+++ front/src/Components/Header.js
@@ -4,6 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import { loginActions } from "../Reducers/loginReducer";
 import { BasicBlueButton } from "../Styles/Buttons"
+import { SearchBoxDiv, SearchInput } from "../Styles/Divs";
 import { ReactComponent as SearchIcon } from "../assets/searchIcon.svg";
 import { ReactComponent as InboxIcon } from "../assets/inboxIcon.svg";
 import { ReactComponent as AchiveIcon } from "../assets/achiveIcon.svg";
@@ -35,7 +36,7 @@ const Containerheader = styled.header`
       align-items: center;
       height: 100%;
     }
-    ul:nth-child(2) {
+    > ul:nth-child(2) {
       padding: 2px 0;
       li:not(:nth-child(2)) {
         display: ${props => props.login.login ? "none" : "block"};
@@ -44,7 +45,7 @@ const Containerheader = styled.header`
         }
       }
     }
-    ul:nth-child(4) {
+    > ul:nth-child(4) {
       gap: 4px;
       list-style: none;
       padding-right: 12px;
@@ -113,42 +114,6 @@ const HeadTextTabLi = styled(HeadTabLi)`
   }
   @media only screen and (max-width: 640px) {
     font-size: 11px;
-  }
-`
-const SearchInput = styled.input`
-  flex: 1;
-  display: block;
-  width: 100%;
-  padding: 0.6em 0.7em;
-  padding-left:32px;
-  color: var(--black-700);
-  line-height: calc(15/13);
-  border: 1px solid var(--black-200);
-  border-radius: 3px;
-  font-size: 13px;
-  background-color: var(--white);
-  outline: 0;
-  :focus {
-    border-color: var(--blue-300);
-    box-shadow: 0 0 0 4px hsla(206, 100%, 40%, .15);
-  }
-`
-const SearchBoxDiv = styled.div`
-  flex-grow: 1;
-  position: relative;
-  margin: 0 8px;
-  > svg {
-    position: absolute;
-    top: 50%;
-    right: auto;
-    left: 0.7em;
-    fill: var(--black-400);
-    transform: translateY(-50%);
-    pointer-events: none;    
-  }
-
-  @media only screen and (max-width: 640px) {
-    display: none !important;
   }
 `
 const MenubarLi = styled(HeadLogoTabLi)`
--- front/src/Pages/Question.js
+++ front/src/Pages/Question.js
@@ -1,6 +1,96 @@
+import styled from "styled-components";
+import { BasicBlueButton } from "../Styles/Buttons";
+import Aside from "../Components/Aside";
+
+const QuestionContainerDiv = styled.div`
+  display: table;
+  > div {
+    display: flex;
+  }
+  > div:nth-child(2) {
+    border-bottom: 1px solid var(--black-075);
+    margin-bottom: 16px;
+    padding-bottom: 8px;
+  }
+  > div:nth-child(3) {
+    float: left;
+    width: calc(100% - 326px);
+  }
+  > div:nth-child(4) {
+    float: right;
+  }
+
+  @media only screen and (max-width: 980px) {
+    div {
+    float: none !important;
+  }
+}
+
+`
+const QuestionDetailDiv = styled.div`
+  white-space: nowrap;
+  margin-bottom: 8px;
+  margin-right: 16px;
+  font-size: 13px;
+  > span:first-child {
+    color: var(--black-500);
+    margin-right: 2px;
+  }
+`
+const QuestionMain = styled.main`
+  display: flex;
+  flex-direction: column;
+`
+const QAWrapDiv = styled.div`
+  display: grid;
+  grid-template-columns: max-content 1fr;
+`
+
+
+
 function Question() {
+  // TODO 날짜 계산기 만들기 today, yesterday, 2 days ago~ 한달, 3 months ago ... */
+  const calculateDate = (date) => {
+    return date
+  }
+
   return (
-    <div>Question</div>
+    <QuestionContainerDiv>
+      <div>
+        <h1>(제목)Cursor Resize on scroll bar of div</h1>
+        <BasicBlueButton to="/questions/ask">Ask Question</BasicBlueButton>
+      </div>
+      <div>
+        <QuestionDetailDiv><span>Asked</span><span>{calculateDate("today")}</span></QuestionDetailDiv>
+        {/* TODO 가장 최근에 달린 답변의 날짜 */}
+        <QuestionDetailDiv><span>Modified</span><span>{calculateDate("today")}</span></QuestionDetailDiv>
+        <QuestionDetailDiv><span>Viewed</span><span>{"7"} times</span></QuestionDetailDiv>
+      </div>
+      <QuestionMain>
+        <div>
+          <QAWrapDiv>
+            <div>보트 구역</div>
+            <div>질문 구역</div>
+            <span>알수 없음</span>
+            <div>코멘트 구역</div>
+          </QAWrapDiv>
+        </div>
+        <div>
+          답변 블록
+          <div>6 Answers 필터 생략</div>
+          <QAWrapDiv>첫 번째 답변 map함수</QAWrapDiv>
+          <QAWrapDiv>두 번째 답변</QAWrapDiv>
+          <div>답변 작성 구역</div>
+          <div>(비로그인 시) 로그인 디브</div>
+          <div>
+            <BasicBlueButton to="/questions/detail">Post your Answer</BasicBlueButton>
+            <p>(비로그인 시)By clicking "Post Your Answer", you agree to our terms of service, privacy policy and cookie policy</p>
+          </div>
+        </div>
+      </QuestionMain>
+      <Aside>
+      </Aside>
+    </QuestionContainerDiv>
   );
 }
 
