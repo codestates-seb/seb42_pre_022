355a329c1650b3a417ae4960bae976034f54717a
YUNH7 <yunny126@gmail.com>
2023-02-21 16:39:43 +0900

    [design] Question 레이아웃 구현 완료

--- front/src/Components/UserCard.js
+++ front/src/Components/UserCard.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 const CardDiv = styled.div`
   display: flex;
   margin-top: 3px;
+  font-size: 12px;
+  min-width: 200px;
   > img {
     height: 32px;
     width: 32px;
@@ -13,27 +15,17 @@ const CardDiv = styled.div`
     margin-left: 8px;
     width: calc(100% - 64px);
   }
-  .username {
-    font-size: 12px;
-    color: var(--blue);
-  }
-  .username:hover {
-    color: var(--blue-500);
-  }
   .reputation {
     font-weight: bold;
   }
 `
-
-
-
 function UserCard() {
 
   return (
     < CardDiv className="profile" >
       <img src={`${process.env.PUBLIC_URL}/images/profileIcon.png`} />
       <div>
-        <p className="username">이름</p>
+        <p className="linktext">이름</p>
         <p className="reputation">명성</p>
       </div>
     </CardDiv >
