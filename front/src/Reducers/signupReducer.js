5c098b2d208718eb1388eb5584845d03d93e8e33
yjyaang <topbuty97@gmail.com>
2023-02-23 21:19:12 +0900

    [feat] login, signup 유효성 검사 구현

--- front/src/Reducers/signupReducer.js
+++ front/src/Reducers/signupReducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 // 이렇게 설정하면 tag빼고는 보존이 가능
-let initialState = {displaynameValue: "", emailValue: "", passwordValue: ""};
+let initialState = {displaynameValue: "", emailValue: "", passwordValue: "", loginEmail: "", loginPassword:""};
 
 const signupSlice = createSlice({
   name: 'signup',
@@ -14,6 +14,12 @@ const signupSlice = createSlice({
     },
     changePasswordValue: (state, action) => {
       state.passwordValue = action.payload.data;
+    },
+    changeLoginEmail: (state, action) => {
+      state.loginEmail = action.payload.data;
+    },
+    changeLoginPassword: (state, action) => {
+      state.loginPassword = action.payload.data;
     },
   }
 })
