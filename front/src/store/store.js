c417719e3223020eaa234f178883dc962bee2d88
YUNH7 <yunny126@gmail.com>
2023-02-24 00:08:19 +0900

    [feat] Question question RUD, answer CUD 구현

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "../Reducers/loginReducer";
 import askquestionReducer from "../Reducers/askquestionReducer";
 import filterquestionReducer from "../Reducers/filterquestionReducer";
+import editPostReducer from "../Reducers/editPostReducer";
 
 const store = configureStore({
   reducer: {
     loginReducer,
     askquestionReducer,
     filter: filterquestionReducer,
+    editPostReducer,
   }
 })
 
