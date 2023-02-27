c7939a134986a86a571a142ef945ed3392df94d4
YUNH7 <yunny126@gmail.com>
2023-02-27 20:03:44 +0900

    [fix] EditPage question 참조 수정

--- front/src/Components/QandAPost.js
+++ front/src/Components/QandAPost.js
@@ -122,8 +122,6 @@ function QandAPost({ question, answer, qwriter }) {
   const dispatch = useDispatch()
   const post = answer ? answer : question
   const id = answer ? answer.answerId : question.questionId
-  const userId = answer ? answer.user.userId : question.userId
-  const displayName = answer ? answer.user.displayName : question.displayName
 
   const saveAnswerToEdit = () => {
     dispatch(editPostActions.changeNowA(answer))
@@ -161,9 +159,9 @@ function QandAPost({ question, answer, qwriter }) {
         <WriterRelatedDiv>
           <div className="qapost">
             <a>Share</a>
-            {userInfo?.userId === userId ? <Link onClick={answer ? saveAnswerToEdit : null} to={`/${answer ? "answers" : "questions"}/${id}/edit`}>Edit</Link> : null}
+            {userInfo?.userId === post.user.userId ? <Link onClick={answer ? saveAnswerToEdit : null} to={`/${answer ? "answers" : "questions"}/${id}/edit`}>Edit</Link> : null}
             <a>Follow</a>
-            {userInfo?.userId === userId ? <a onClick={deletePost}>Delete</a> : null}
+            {userInfo?.userId === post.user.userId ? <a onClick={deletePost}>Delete</a> : null}
           </div>
           {post.modifiedDate ?
             <WriterCardDiv >
@@ -171,9 +169,9 @@ function QandAPost({ question, answer, qwriter }) {
                 edited {dateTimeFormat(post.modifiedDate)}</span>
             </WriterCardDiv>
             : null}
-          <WriterCardDiv iswriter={qwriter === userId ? 1 : null}>
+          <WriterCardDiv iswriter={qwriter === post.user.userId ? 1 : null}>
             <div>asked {dateTimeFormat(post.createdDate)}</div>
-            <UserCard username={displayName} reputation={"100"} />
+            <UserCard username={post.user.displayName} reputation={post.user.reputation} />
           </WriterCardDiv>
         </WriterRelatedDiv>
       </div>
