beaec81e5aa863840665638165cb0d7c8f0795d2
YUNH7 <yunny126@gmail.com>
2023-02-27 16:15:10 +0900

    [fix] util/요청함수 config값 수정

--- front/src/util/deleteData.js
+++ front/src/util/deleteData.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const config = {
-  headers: {
-    "Authorization": JSON.parse(sessionStorage.getItem("accessToken"))
-  }
-}
-
 async function deleteData(url) {
+  const config = {
+    headers: {
+      "Authorization": JSON.parse(sessionStorage.getItem("accessToken"))
+    }
+  }
   try {
     const response = await axios.delete(`${process.env.REACT_APP_API_URL}${url}`, config)
     return response.data
--- front/src/util/patchData.js
+++ front/src/util/patchData.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const config = {
-  headers: {
-    "Authorization": JSON.parse(sessionStorage.getItem("accessToken"))
-  }
-}
-
 async function patchData(url, data) {
+  const config = {
+    headers: {
+      "Authorization": JSON.parse(sessionStorage.getItem("accessToken"))
+    }
+  }
   try {
     const response = await axios.patch(`${process.env.REACT_APP_API_URL}${url}`, data, config)
     return response.data
